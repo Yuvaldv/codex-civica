ad1797520dd8b6eb167d88fbd06784988a8d86a7
Yuvaldv <Yuvaldv@users.noreply.github.com>
2026-05-15 11:49:45 +0000

    deploy: 9af7ee9fdcfa6df50a03e1a8ce76e90f7d974bd3

--- assets/js/acecf23e.8d2100d9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([["3220"],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
